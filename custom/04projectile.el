c579f172d7272a1f326544e9f8192d9dee61e77e
Paul D. & Björn S <bjorn@def-end.se>
2014-02-15 13:28:37 -0600

    Add several plugins with their configurations

---
+++ custom/04projectile.el
@@ -0,0 +1,4 @@
+(require 'grizzl)
+(projectile-global-mode)
+(setq projectile-enable-caching t)
+(setq projectile-completion-system 'grizzl)
---
+++ custom/99auto-complete.el
@@ -0,0 +1,7 @@
+ (require 'auto-complete-config)
+ (add-to-list 'ac-dictionary-directories
+     "~/.emacs.d/.dict")
+ (ac-config-default)
+ (setq ac-ignore-case nil)
+ (add-to-list 'ac-modes 'enh-ruby-mode)
+ (add-to-list 'ac-modes 'web-mode)
