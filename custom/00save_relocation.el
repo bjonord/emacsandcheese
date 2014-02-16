a064b9c5bc50251612727f87d34684cce1190395
Björn Skarner <bjorn@def-end.se>
2014-02-16 16:23:38 +0100

    Rearrange custom files

--- custom/00save_relocation.el
+++
@@ -1,7 +0,0 @@
-(setq backup-by-copying t
-      backup-directory-alist '(("." . "~/.emacs.d/.saves"))
-      delete-old-versions t
-      kept-new-versions 6
-      kept-old-versions 2
-      version-control t)
-
