33049cf08601a22e3ffd8ef03e37089a687b4a30
bjoska <bjorn.skarner@it.cdon.com>
2014-03-07 10:19:10 +0100

    Add windmove keys to org-mode

---
+++ enc_config/org-mode-conf.el
@@ -0,0 +1,4 @@
+(add-hook 'org-shiftup-final-hook 'windmove-up)
+(add-hook 'org-shiftleft-final-hook 'windmove-left)
+(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
