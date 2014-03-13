bc56b6b464ceceaa31c3b32e0de9150209cc0f2c
Björn Skarner <bjorn@def-end.se>
2014-03-13 21:37:21 +0100

    Add several key commands and functions
    Addition of several commands and functions related to the same.
    
    Also renamed the keybindings file with a better namespace.

---
+++ enc_config/enc-keybindings.el
@@ -0,0 +1,20 @@
+;; Scroll other window up and down
+(global-set-key (kbd "C-M-v") 'enc-scroll-other-window-up)
+(global-set-key (kbd "C-M-m") 'enc-scroll-other-window-down)
+
+;; Moving lines up and down.
+(global-set-key [(meta shift up)]  'enc-move-line-up)
+(global-set-key [(meta shift down)]  'enc-move-line-down)
+
+;; In place to prevent me from closing emacs.
+(global-unset-key (kbd "C-x C-c"))
+(global-unset-key (kbd "C-x C-z"))
+(global-set-key (kbd "C-c C-q") 'save-buffers-kill-terminal)
+(global-set-key (kbd "C-c C-e") 'suspend-frame)
+
+;; Query the current selection in Duck Duck Go
+(global-set-key (kbd "C-c C-s") 'enc-duck-d-g)
+
+;; Quick commands to indent and clean.
+(global-set-key (kbd "C-c C-i") 'enc-indent-region-or-buffer)
+(global-set-key (kbd "C-c C-n") 'enc-clean-buffer)
