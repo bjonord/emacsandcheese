0ef48d21153024f36392eca86a02b81cf6cff419
bjoska <bjorn.skarner@it.cdon.com>
2014-05-16 10:45:33 +0200

    Add isearchb key binding to initialization

--- enc_config/00-general-setup.el
+++ enc_config/00-general-setup.el
@@ -61,3 +61,6 @@
       ;; We remove Which Function Mode from the mode line, because it's mostly
       ;; invisible here anyway.
       (assq-delete-all 'which-func-mode mode-line-misc-info))
+
+;; Define a key binding for isearchb.
+(define-key global-map [(control ?q)] 'isearchb-activate)
